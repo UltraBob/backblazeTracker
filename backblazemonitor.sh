c38deddecc48950ea7c601adc067644a9f2e7b24
Bob McDonald <bob.tdlc@gmail.com>
2014-07-29 18:26:03 +0900

    added a configuration for the frequency

--- backblazemonitor.sh
+++ backblazemonitor.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-FREQUENCY=60
+
+FREQUENCY=1
+if [ $1 ]
+then
+    FREQUENCY=$1
+fi
+SLTIME=$(expr "$FREQUENCY" \* 60)
+echo $SLTIME
 VOLUME=$(sed -n 's/^.*scratch_mountpoint="\(.*\)".*$/\1/p' /Library/Backblaze.bzpkg/bzdata/bzinfo.xml)
 
 CHKTIME=$(date +'%R')
@@ -8,7 +15,8 @@ REPORTINGTIME=$(date)
 CHKFILE=$(sed -n 's/^.*bzfname="\(.*\)".*$/\1/p' "$VOLUME"/.bzvol/bzscratch/bzcurrentlargefile/currentlargefile.xml)
 FILESIZE=$(du -h "$CHKFILE"| awk '{print $1}')
 echo $CHKTIME $SPACEREMAINING / $FILESIZE remaining of $CHKFILE
-while sleep $FREQUENCY
+echo "You will receive an update every $FREQUENCY minutes"
+while sleep "$SLTIME"
 do 
     CHKTIME=$(date +'%R')
     SPACEREMAINING=$(du -h -d 0 "$VOLUME"/.bzvol/bzscratch/bzcurrentlargefile | awk '{print $1}')
