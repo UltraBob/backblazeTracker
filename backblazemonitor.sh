7a9d515d89a8e0b97718b10ab43012364a12f2c2
Bob McDonald <bob.tdlc@gmail.com>
2014-07-29 18:00:12 +0900

    formatting

--- backblazemonitor.sh
+++ backblazemonitor.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+FREQUENCY=60
 VOLUME=$(sed -n 's/^.*scratch_mountpoint="\(.*\)".*$/\1/p' /Library/Backblaze.bzpkg/bzdata/bzinfo.xml)
 
-CHKTIME=$(date +'%R');SPACEREMAINING=$(du -h -d 0 "$VOLUME"/.bzvol/bzscratch/bzcurrentlargefile | awk '{print $1}');REPORTINGTIME=$(date);CHKFILE=$(sed -n 's/^.*bzfname="\(.*\)".*$/\1/p' "$VOLUME"/.bzvol/bzscratch/bzcurrentlargefile/currentlargefile.xml);FILESIZE=$(du -h "$CHKFILE"| awk '{print $1}'); echo $CHKTIME $SPACEREMAINING / $FILESIZE remaining of $CHKFILE; while sleep 60; do CHKTIME=$(date +'%R');SPACEREMAINING=$(du -h -d 0 "$VOLUME"/.bzvol/bzscratch/bzcurrentlargefile | awk '{print $1}');REPORTINGTIME=$(date);CHKFILE=$(sed -n 's/^.*bzfname="\(.*\)".*$/\1/p' "$VOLUME"/.bzvol/bzscratch/bzcurrentlargefile/currentlargefile.xml);FILESIZE=$(du -h "$CHKFILE"| awk '{print $1}'); echo $CHKTIME $SPACEREMAINING / $FILESIZE remaining of $CHKFILE; done
+CHKTIME=$(date +'%R')
+SPACEREMAINING=$(du -h -d 0 "$VOLUME"/.bzvol/bzscratch/bzcurrentlargefile | awk '{print $1}')
+REPORTINGTIME=$(date)
+CHKFILE=$(sed -n 's/^.*bzfname="\(.*\)".*$/\1/p' "$VOLUME"/.bzvol/bzscratch/bzcurrentlargefile/currentlargefile.xml)
+FILESIZE=$(du -h "$CHKFILE"| awk '{print $1}')
+echo $CHKTIME $SPACEREMAINING / $FILESIZE remaining of $CHKFILE
+while sleep 60
+do 
+    CHKTIME=$(date +'%R')
+    SPACEREMAINING=$(du -h -d 0 "$VOLUME"/.bzvol/bzscratch/bzcurrentlargefile | awk '{print $1}')
+    REPORTINGTIME=$(date)
+    CHKFILE=$(sed -n 's/^.*bzfname="\(.*\)".*$/\1/p' "$VOLUME"/.bzvol/bzscratch/bzcurrentlargefile/currentlargefile.xml)
+    FILESIZE=$(du -h "$CHKFILE"| awk '{print $1}')
+    echo $CHKTIME $SPACEREMAINING / $FILESIZE remaining of $CHKFILE
+done
