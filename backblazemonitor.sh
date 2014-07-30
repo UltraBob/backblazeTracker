ab5b60532ed94506f9f5c9c635f2a8f1e981eca8
Bob McDonald <bob.tdlc@gmail.com>
2014-07-30 17:16:29 +0900

    added colors and color configuration to the output

--- backblazemonitor.sh
+++ backblazemonitor.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
 
 FREQUENCY=1
+COLORTIME="\033[31m"
+COLORTEXT="\033[32m"
+COLORSPACE="\033[34m"
+COLORFILE="\033[36m"
 if [ $1 ]
 then
     FREQUENCY=$1
 fi
-SLTIME=$(expr "$FREQUENCY" \* 60)
-echo $SLTIME
-VOLUME=$(sed -n 's/^.*scratch_mountpoint="\(.*\)".*$/\1/p' /Library/Backblaze.bzpkg/bzdata/bzinfo.xml)
 
-CHKTIME=$(date +'%R')
-SPACEREMAINING=$(du -h -d 0 "$VOLUME"/.bzvol/bzscratch/bzcurrentlargefile | awk '{print $1}')
-REPORTINGTIME=$(date)
+SLTIME=$(expr "$FREQUENCY" \* 60)
+VOLUME=$(sed -n 's/^.*scratch_mountpoint="\(.*\)".*$/\1/p' /Library/Backblaze.bzpkg/bzdata/bzinfo.xml)
 CHKFILE=$(sed -n 's/^.*bzfname="\(.*\)".*$/\1/p' "$VOLUME"/.bzvol/bzscratch/bzcurrentlargefile/currentlargefile.xml)
-FILESIZE=$(du -h "$CHKFILE"| awk '{print $1}')
-echo $CHKTIME $SPACEREMAINING / $FILESIZE remaining of $CHKFILE
 echo "You will receive an update every $FREQUENCY minutes"
-while sleep "$SLTIME"
+while [ 1 ]
 do 
     CHKTIME=$(date +'%R')
     SPACEREMAINING=$(du -h -d 0 "$VOLUME"/.bzvol/bzscratch/bzcurrentlargefile | awk '{print $1}')
-    REPORTINGTIME=$(date)
-    CHKFILE=$(sed -n 's/^.*bzfname="\(.*\)".*$/\1/p' "$VOLUME"/.bzvol/bzscratch/bzcurrentlargefile/currentlargefile.xml)
-    FILESIZE=$(du -h "$CHKFILE"| awk '{print $1}')
-    echo $CHKTIME $SPACEREMAINING / $FILESIZE remaining of $CHKFILE
+    if [ test -e "$CHKFILE" ]
+    then
+        FILESIZE=$(du -h "$CHKFILE"| awk '{print $1}')
+    else
+        FILESIZE="?? (file currently unavailable)"
+    fi
+    echo "$COLORTIME$CHKTIME $COLORSPACE$SPACEREMAINING$COLORTEXT /$COLORSPACE$FILESIZE$COLORTEXT remaining of $COLORFILE$CHKFILE"
+    sleep "$SLTIME"
 done
