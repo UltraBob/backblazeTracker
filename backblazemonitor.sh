77e0c1e10aa8866f14dd912a61ac14cb1346bda6
Bob McDonald <bob.tdlc@gmail.com>
2014-07-31 10:01:52 +0900

    fixed the script to work with local scratch disk.  Fixes https://github.com/UltraBob/backblazeTracker/issues/2

--- backblazemonitor.sh
+++ backblazemonitor.sh
@@ -13,13 +13,24 @@ fi
 
 SLTIME=$(expr "$FREQUENCY" \* 60)
 VOLUME=$(sed -n 's/^.*scratch_mountpoint="\(.*\)".*$/\1/p' /Library/Backblaze.bzpkg/bzdata/bzinfo.xml)
-CHKFILE=$(sed -n 's/^.*bzfname="\(.*\)".*$/\1/p' "$VOLUME"/.bzvol/bzscratch/bzcurrentlargefile/currentlargefile.xml)
+if [ -f /Library/Backblaze.bzpkg/bzdata/bzbackup/bzdatacenter/bzcurrentlargefile/currentlargefile.xml ]
+then
+    echo "local scratch"
+    CHKFILE=$(sed -n 's/^.*bzfname="\(.*\)".*$/\1/p' /Library/Backblaze.bzpkg/bzdata/bzbackup/bzdatacenter/bzcurrentlargefile/currentlargefile.xml)
+    SPACECHECK=/Library/Backblaze.bzpkg/bzdata/bzbackup/bzdatacenter/bzcurrentlargefile
+    FILEFORSIZE=/Library/Backblaze.bzpkg/bzdata/bzbackup/bzdatacenter/bzcurrentlargefile/currentlargefile.xml
+else
+    echo "external scratch"
+    CHKFILE=$(sed -n 's/^.*bzfname="\(.*\)".*$/\1/p' "$VOLUME".bzvol/bzscratch/bzcurrentlargefile/currentlargefile.xml)
+    SPACECHECK="$VOLUME"/.bzvol/bzscratch/bzcurrentlargefile
+    FILEFORSIZE="$VOLUME".bzvol/bzscratch/bzcurrentlargefile/currentlargefile.xml
+fi
 echo "You will receive an update every $FREQUENCY minutes"
 while [ 1 ]
 do 
     CHKTIME=$(date +'%R')
-    SPACEREMAINING=$(du -h -d 0 "$VOLUME"/.bzvol/bzscratch/bzcurrentlargefile | awk '{print $1}')
-    FILESIZE=$((`sed -n 's/^.*numbytesinfile="\([^\"]*\)".*$/\1/p' "$VOLUME"/.bzvol/bzscratch/bzcurrentlargefile/currentlargefile.xml`/1024**2))"M"
+    SPACEREMAINING=$(du -h -d 0 "$SPACECHECK" | awk '{print $1}')
+    FILESIZE=$((`sed -n 's/^.*numbytesinfile="\([^\"]*\)".*$/\1/p' "$FILEFORSIZE"`/1024**2))"M"
     echo "$COLORTIME$CHKTIME $COLORSPACE$SPACEREMAINING$COLORTEXT / $COLORSPACE$FILESIZE$COLORTEXT remaining of $COLORFILE$CHKFILE"
     sleep "$SLTIME"
 done
